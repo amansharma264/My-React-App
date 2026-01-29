27d485b8791e2a59db6c8dc930fd0171c83f00d7
amansharma264 <amankumar170789@gmail.com>
2026-01-29 18:52:03 +0530

    updating the 03-multiple-effect files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import UseEffectBasics from './tutorial/02-useEffect/starter/02-useEffect-basics'
+import MultipleEffects from './tutorial/02-useEffect/starter/03-multiple-effects'
 import Final from './tutorial/01-useState/final/03-useState-array';
 
 function App() {
   return (
     <div className='container'>
-      <UseEffectBasics />
+      <MultipleEffects />
       
     </div>
   )
