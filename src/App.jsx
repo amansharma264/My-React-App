6584c3e259754f54f71518fa32bef7037e82a0a6
amansharma264 <amankumar170789@gmail.com>
2026-01-29 18:26:59 +0530

    updating useEffect-01-code-examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import UseStateGotcha from './tutorial/01-useState/starter/05-useState-gotcha'
+import CodeExample from './tutorial/02-useEffect/starter/01-code-example'
 import Final from './tutorial/01-useState/final/03-useState-array';
 
 function App() {
   return (
     <div className='container'>
-      <UseStateGotcha />
+      <CodeExample />
       
     </div>
   )
