c47ad2aed6164f3f05d137be5cae0a77d4104f5e
amansharma264 <amankumar170789@gmail.com>
2026-01-29 10:52:04 +0530

    update useState

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,18 +1,30 @@
 import { data } from "../../../data";
 
-import React, {useState} from "react";
+import React from "react";
 const UseStateArray = () => { 
   const [people, setPeople] = React.useState(data);
 
+  const removeItem = (id) =>{
+    console.log(id);
+    // const newPeople = people.filter((person)=>person.id !== id);
+    setPeople(people.filter((person)=> person.id !== id));
+  }
+
+  const clearAllItems = () =>{
+    setPeople([]);
+  }
   return (
   <div>
     {people.map((person) =>{
       const {id, name} = person;
       // console.log(person);
-      return <div key={id}>
+      return( <div key={id}>
         <h4>{name}</h4>
+        <button type="button" onClick={()=> removeItem(id)}>remove</button>
       </div>
+      );
     })}
+    <button type="button" style={{marginTop: '2rem'}} className="'btn" onClick={()=> setPeople([])}>clear items</button>
   </div>
   );
 };
