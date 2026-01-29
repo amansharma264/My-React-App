f641619b0efce352e2dc925c9345e74e18cb1c6b
amansharma264 <amankumar170789@gmail.com>
2026-01-29 12:03:58 +0530

    update 05-useState-gotcha

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import UseStateObject from './tutorial/01-useState/starter/04-useState-object'
+import UseStateGotcha from './tutorial/01-useState/starter/05-useState-gotcha'
 import Final from './tutorial/01-useState/final/03-useState-array';
 
 function App() {
   return (
     <div className='container'>
-      <UseStateObject />
+      <UseStateGotcha />
       
     </div>
   )
