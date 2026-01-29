1788bdfc9dd8d8455c11306158c93572dc89f6b4
amansharma264 <amankumar170789@gmail.com>
2026-01-29 11:40:36 +0530

    updating useState-object files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import UseStateArray from './tutorial/01-useState/starter/03-useState-array'
+import UseStateObject from './tutorial/01-useState/starter/04-useState-object'
 import Final from './tutorial/01-useState/final/03-useState-array';
 
 function App() {
   return (
     <div className='container'>
-      <UseStateArray />
+      <UseStateObject />
       
     </div>
   )
