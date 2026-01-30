f1b7f3c0b4daf58d3577c8a0bc3d2838be6a60de
amansharma264 <amankumar170789@gmail.com>
2026-01-30 16:12:42 +0530

    updating files 01-multiple-returns-basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import FetchData from './tutorial/02-useEffect/starter/04-fetch-data'
+import MultipleReturnsBasics from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics'
 import Final from './tutorial/01-useState/final/03-useState-array';
 
 function App() {
   return (
     <div className='container'>
-      <FetchData />
+      <MultipleReturnsBasics />
       
     </div>
   )
