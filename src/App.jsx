9ca426f1df31b9c187b28df079ecf1547583e3bf
amansharma264 <amankumar170789@gmail.com>
2026-01-30 16:20:05 +0530

    update the fetch data from url 03-fetch-data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import MultipleReturnsBasics from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics'
+import MultipleReturnsFetchData from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data'
 import Final from './tutorial/01-useState/final/03-useState-array';
 
 function App() {
   return (
     <div className='container'>
-      <MultipleReturnsBasics />
+      <MultipleReturnsFetchData />
       
     </div>
   )
--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,24 @@
-import { useEffect, useState } from 'react';
+import { use, useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(()=>{
+    const fetchUser = async () =>{
+      try {
+        const response = await fetch(url);
+        const user = await response.json();
+        console.log(user);
+        
+      } catch (error) {
+        console.log(error);
+        
+      }
+    };
+    fetchUser();
+  }, [])
+
   return <h2>Fetch Data </h2>;
 };
 export default MultipleReturnsFetchData;
