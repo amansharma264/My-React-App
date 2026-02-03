e1abe0510c0792b917e545b0c9b853a109ef1467
amansharma264 <amankumar170789@gmail.com>
2026-02-03 15:48:19 +0530

    update 06-toggle challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import ShortCircuitExamples from './tutorial/03-conditional-rendering/starter/05-short-circuit-examples'
+import ToggleChallenge from './tutorial/03-conditional-rendering/starter/06-toggle-challenge'
 import Final from './tutorial/01-useState/final/03-useState-array';
 
 function App() {
   return (
     <div className='container'>
-      <ShortCircuitExamples />
+      <ToggleChallenge />
       
     </div>
   )
