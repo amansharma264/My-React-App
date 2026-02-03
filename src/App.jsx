1ebc1840e3b648d44537ee8487d76afac3a3970a
amansharma264 <amankumar170789@gmail.com>
2026-02-03 15:02:25 +0530

    updating 04-short-circuit-overview

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Example from './tutorial/03-conditional-rendering/starter/03-hooks-rule'
+import ShortCircuitOverview from './tutorial/03-conditional-rendering/starter/04-short-circuit-overview'
 import Final from './tutorial/01-useState/final/03-useState-array';
 
 function App() {
   return (
     <div className='container'>
-      <Example />
+      <ShortCircuitOverview />
       
     </div>
   )
