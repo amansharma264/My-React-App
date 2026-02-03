2f3c63f6f821269490ac25b51d21e36d09285f3c
amansharma264 <amankumar170789@gmail.com>
2026-02-03 16:26:59 +0530

    update cleanup-function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import ToggleChallenge from './tutorial/03-conditional-rendering/starter/06-toggle-challenge'
+import CleanupFunction from './tutorial/02-useEffect/starter/05-cleanup-function'
 import Final from './tutorial/01-useState/final/03-useState-array';
 
 function App() {
   return (
     <div className='container'>
-      <ToggleChallenge />
+      <CleanupFunction />
       
     </div>
   )
