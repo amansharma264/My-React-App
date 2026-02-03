e9aa6ee57355f81553d3a80bb5c544abd076a837
amansharma264 <amankumar170789@gmail.com>
2026-02-03 14:42:07 +0530

    update 03-hooks-rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import MultipleReturnsFetchData from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data'
+import Example from './tutorial/03-conditional-rendering/starter/03-hooks-rule'
 import Final from './tutorial/01-useState/final/03-useState-array';
 
 function App() {
   return (
     <div className='container'>
-      <MultipleReturnsFetchData />
+      <Example />
       
     </div>
   )
