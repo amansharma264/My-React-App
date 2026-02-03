0d33ce494a576f69e7cb6e077d8e4e11c7899543
amansharma264 <amankumar170789@gmail.com>
2026-02-03 14:36:55 +0530

    update the 02-multiple-returns-fetch

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -6,10 +6,14 @@ const MultipleReturnsFetchData = () => {
   const [isError, setIsError] = useState(false);
   const [user, setUser] = useState(null)
 
-  useEffect(()=>{
-    const fetchUser = async () =>{
+const fetchUser = async () =>{
       try {
         const response = await fetch(url);
+        if(!response.ok){
+          setIsError(true);
+          setIsLoading(false);
+          return
+        }
         const user = await response.json();
         // console.log(user);
         setUser(user);
@@ -20,6 +24,8 @@ const MultipleReturnsFetchData = () => {
       }
       setIsLoading(false);
     };
+
+  useEffect(()=>{
     fetchUser();
   }, []);
 
@@ -32,11 +38,13 @@ const MultipleReturnsFetchData = () => {
     return <h2>There was an error</h2>
   }
 
+  const {avatar_url, name, company, bio} = user;
+
   return <div>
-     <img style={{width: '150px', borderRadius: '25px'}} src={user.avatar_url} alt= {user.name} />
-     <h2>{user.name}</h2>
-     <h4>Works at{user.company}</h4>
-     <p>{user.bio}</p>
+     <img style={{width: '150px', borderRadius: '25px'}} src={avatar_url} alt= {name} />
+     <h2>{name}</h2>
+     <h4>Works at{company}</h4>
+     <p>{bio}</p>
     </div>;
 };
 export default MultipleReturnsFetchData;
