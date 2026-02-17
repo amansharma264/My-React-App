69e54282d67f7f50f9838b34100bd550fc87d700
amansharma264 <amankumar170789@gmail.com>
2026-02-17 09:58:03 +0530

    add components and structure for tutorial examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import CleanupFunction from './tutorial/02-useEffect/starter/05-cleanup-function'
-import Final from './tutorial/01-useState/final/03-useState-array';
-
+import List from "./tutorial/05-leverage-javascript/starter/List";
 function App() {
   return (
     <div className='container'>
-      <CleanupFunction />
-      
+      <div>
+        <h2>Leverage Javascript</h2>
+        <List/>
+      </div>
     </div>
-  )
+  );
 }
 export default App
--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 const CleanupFunction = () => {
   const [toggle, setToggle] = useState(false);
+  console.log('render');
   return (
     <div>
       <button className="btn" onClick={()=> setToggle(!toggle)}>
@@ -14,15 +15,11 @@ const CleanupFunction = () => {
 
 const RandomComponent = ()=>{
   useEffect(()=>{
-    console.log('hmm, this is interesting');
-    const intId = setInterval(()=>{
-      console.log('hello from interval');
-    }, 1000)
-    return () =>{
-      clearInterval(intId);
-      console.log('cleanup');
-      
+    const someFunc = ()=>{
+      // some logic
     }
+    window.addEventListener('scroll', someFunc);
+    return () => window.removeEventListener('scroll', someFunc);
   }, [])
   return (
     <h1>Hello there</h1>
