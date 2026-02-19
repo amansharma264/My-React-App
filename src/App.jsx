dc46d34edf6b31d3d8e8ea7c933248d47a03b6b4
amansharma264 <amankumar170789@gmail.com>
2026-02-19 14:55:33 +0530

    replace OtherInputs with UncontrolledInputs component and implement form submission handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import OtherInputs from "./tutorial/06-forms/starter/04-other-inputs"
+import UncontrolledInputs from "./tutorial/06-forms/starter/05-form-data"
 
 import React from 'react'
 
 const App = () => {
   return (
     <div className="container">
-      <OtherInputs />
+      <UncontrolledInputs />
     </div>
   )
 }
