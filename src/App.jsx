232616993339fd8620ed7ad5c8499793955a8efc
amansharma264 <amankumar170789@gmail.com>
2026-03-01 11:28:28 +0530

    implement useReducer for state management in ReducerBasics component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Navbar from './tutorial/09-context-api/starter/Navbar'
+import ReducerBasics from './tutorial/10-useReducer/starter/01-useReducer'
 
 import React from 'react'
 
 const App = () => {
   return <>
-  <Navbar />
+  <ReducerBasics />
   </>
 }
 
