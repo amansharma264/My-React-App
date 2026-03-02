3aad3e06cb40eb976bde4db04b613884d9ce3bd1
amansharma264 <amankumar170789@gmail.com>
2026-03-02 10:57:26 +0530

    replace LowerState component with LatestReact and implement useTransition for improved performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import LowerState from './tutorial/11-performance/starter/03-hooks'
+import LatestReact from './tutorial/11-performance/starter/04-react-18'
 
 import React from 'react'
 
 const App = () => {
   return <>
-  <LowerState />
+  <LatestReact />
   </>
 }
 
